365d4be4a6d72dad59e0d8faedcdfa0b55e72da3
Ahmed emad <ahmed.emad.23112003@gmail.com>
2023-10-24 13:40:22 +0300

    check word

---
+++ app/src/main/java/com/example/task_android6/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.task_android6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val et_word = findViewById<EditText>(R.id.et_word)
+        val tv_word = findViewById<TextView>(R.id.tv_word)
+        val check = findViewById<Button>(R.id.btn_check)
+        val show = findViewById<Button>(R.id.btn_show)
+        val wrong_word: String = "putcomer"
+
+        tv_word.text = wrong_word
+
+        check.setOnClickListener {
+            val word = et_word.editableText.toString()
+            val congrats: String = "Congratulations"
+            val try_again: String = "Try again!!"
+            val true_word: String = "computer"
+
+            if (word == true_word){
+                tv_word.text = congrats
+            }
+            else {
+                tv_word.text = try_again
+            }
+        }
+
+        show.setOnClickListener {
+            val true_word: String = "computer"
+            tv_word.text = true_word
+        }
+
+
+    }
+}
